c6c7a1f1bbd1263b053cf9b7ea3c5af3e35d963b
mattbesedick <mattbezy@gmail.com>
2020-08-24 01:44:24 -0500

    updated home component with comments to follow along on how to retrieve data in our components

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,18 +6,29 @@ import { Divider } from '@material-ui/core';
 
 
 const Home = (props) => {
+
+    //set state for games
     const [games, setGames] = useState([]);
 
+    //useEffect will run on componentMount, anything in here will be called when page loads/reloads/updates
     useEffect(() => {
+
+        //since useEffect can't be async itself, you have to define an async fuction and call
+        //it inside useEffect. This function will hit our api and retrieve the list of games
+        //in the db
         const fetch = async () => {
+            //I used destructuring here because I knew there was a data object attached to the response
+            //just for readability. Axios is a HTTP client that returns a promise
             const { data } = await Axios.get('/api/games')
-            console.log(data)
+
+            //setting state for the new data retrieved
             setGames(data)
         }
+        //call fetch function 
         fetch()
     }, [])
 
-    console.log(games)
+    //this component doesn't really do much but display the names of the games right now
     if (games.length > 0) {
         return (
             <div>
