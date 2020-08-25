1049a7388ba7b4a83847c1a298236b5e4d566ff3
mattbesedick <mattbezy@gmail.com>
2020-08-25 12:19:03 -0500

    syntax fix

--- src/components/CustomTableCell.js
+++ src/components/CustomTableCell.js
@@ -4,14 +4,15 @@ import { TableRow, TableCell } from '@material-ui/core'
 
 const CustomTableCell = (props) => {
     console.log(props)
-    return (<TableRow>
-        <TableCell align='left'>{props.name}</TableCell>
-        <TableCell align='left'>{props.date}</TableCell>
-        <TableCell align='left'>{props.players.map(name => `${name} `)}</TableCell>
-        <TableCell align='left'>{props.winners.map(name => `${name} `)}</TableCell>
-        <TableCell align='left'>{props.notes}</TableCell>
-        <TableCell align='left'>{props.valid}</TableCell>
-    </TableRow>
+    return (
+        <TableRow>
+            <TableCell align='left'>{props.name}</TableCell>
+            <TableCell align='left'>{props.date}</TableCell>
+            <TableCell align='left'>{props.players.map(name => `${name} `)}</TableCell>
+            <TableCell align='left'>{props.winners.map(name => `${name} `)}</TableCell>
+            <TableCell align='left'>{props.notes}</TableCell>
+            <TableCell align='left'>{props.valid}</TableCell>
+        </TableRow>
     )
 }
 
