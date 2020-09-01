0021174a91acd36d20770c53908bfd689e11437b
mattbesedick <mattbezy@gmail.com>
2020-09-01 14:07:13 -0500

    fixed merge changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 import './App.css';
-=======
 import Nav from './components/Nav'
->>>>>>> 1a71d3ff6fbe54fe6ebbef263b020876611e4995
 import Routes from './Routes';
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
-=======
       <Nav />
->>>>>>> 1a71d3ff6fbe54fe6ebbef263b020876611e4995
       <Routes />
       {/* <div className="App">
        <AppBar />
--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Route } from "react-router-dom";
 import Home from './components/Home';
-<<<<<<< HEAD
-=======
 import TableList from './components/TableList';
 
->>>>>>> 1a71d3ff6fbe54fe6ebbef263b020876611e4995
 
 const Routes = () => {
     return (
         <>
             <Route exact path='/' component={Home} />
-<<<<<<< HEAD
-=======
             <Route path="/List" component={TableList} />
->>>>>>> 1a71d3ff6fbe54fe6ebbef263b020876611e4995
         </>
     );
 }
