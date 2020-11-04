4b543c8b967f26567128ff855082268848726f6d
mattbesedick <mattbezy@gmail.com>
2020-11-04 15:50:57 -0600

    added bar chart for total player wins to home page

---
+++ src/components/PlayerWinsBarChart.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis, Cell } from 'recharts';
+
+const PlayerWinsBarChart = ({data}) => {
+    // const data=[{name: "Matthew", wins: 10}, {name: "Alex", wins: 40}, {name: "Connor", wins: 20}]
+    let wins = []
+    data.map(player => {
+        console.log(player)
+        wins.push({name: player.data.firstName, wins: player.data.totalWins})
+    })
+    const colors = ["#1920CE", "#0CAE2F", "#570697"]
+    return (
+        <ResponsiveContainer width="90%" height="80%" >
+            <BarChart data={wins} margin={{right: 10}}>
+                <CartesianGrid  strokeDasharray="3 3"/>
+                <XAxis dataKey="name" />
+                <YAxis/>
+                <Tooltip />
+                {/* <Legend /> */}
+                <Bar dataKey="wins"> 
+                    {
+                        wins.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={colors[Math.floor(Math.random() * colors.length)]} />
+                        ))
+                    }
+                </Bar>
+            </BarChart>        
+        </ResponsiveContainer>
+        
+     );
+}
+ 
+export default PlayerWinsBarChart;
